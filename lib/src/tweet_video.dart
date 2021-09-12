090167e5240d38f84fbfd4c8b12433842c7a5ade
Steve Gehrman <sgehrman@gmail.com>
2021-09-12 02:49:06 -0700

    fixed video on web

--- lib/src/tweet_video.dart
+++ lib/src/tweet_video.dart
@@ -6,6 +6,7 @@ import 'package:flutter/material.dart';
 import 'package:flutter/services.dart';
 import 'package:tweet_ui/models/viewmodels/tweet_vm.dart';
 import 'package:url_launcher/url_launcher.dart';
+import 'package:video_player/video_player.dart';
 
 class TweetVideo extends StatefulWidget {
   const TweetVideo(
@@ -31,76 +32,107 @@ class _TweetVideoState extends State<TweetVideo>
     with AutomaticKeepAliveClientMixin {
   late BetterPlayerConfiguration betterPlayerConfiguration;
   late BetterPlayerController controller;
+  late VideoPlayerController _vpController;
 
   @override
   void initState() {
     super.initState();
-    betterPlayerConfiguration = BetterPlayerConfiguration(
-      placeholder: const Center(
-        child: SizedBox(
-          height: 32,
-          width: 32,
-          child: CircularProgressIndicator(),
-        ),
-      ),
-      errorBuilder: (context, message) {
-        return const Text('Error while loading video :-(');
-      },
-      aspectRatio: widget.tweetVM.getDisplayTweet().videoAspectRatio,
-      controlsConfiguration: BetterPlayerControlsConfiguration(
-        enablePlaybackSpeed: false,
-        enableSkips: false,
-        enableMute: !widget.tweetVM.getDisplayTweet().hasGif,
-        showControls: !widget.tweetVM.getDisplayTweet().hasGif,
-        enableSubtitles: false,
-        enableFullscreen: widget.enableFullscreen,
-      ),
-      allowedScreenSleep: false,
-      deviceOrientationsOnFullScreen: [
-        DeviceOrientation.portraitUp,
-        DeviceOrientation.landscapeLeft,
-        DeviceOrientation.landscapeRight
-      ],
-      fullScreenAspectRatio: widget.tweetVM.getDisplayTweet().videoAspectRatio,
-      autoPlay: widget.tweetVM.getDisplayTweet().hasGif || widget.autoPlay,
-      looping: widget.tweetVM.getDisplayTweet().hasGif,
-      overlay: Padding(
-        padding: const EdgeInsets.only(
-          left: 4.0,
-        ),
-        child: widget.tweetVM.getDisplayTweet().hasGif
-            ? Align(
-                alignment: Alignment.bottomLeft,
-                child: Image.asset(
-                  'assets/tw__ic_gif_badge.png',
-                  fit: BoxFit.fitWidth,
-                  package: 'tweet_ui',
-                  height: 16,
-                  width: 16,
-                ),
-              )
-            : Container(),
-      ),
-    );
+
     final videoUrl = widget.videoHighQuality!
         ? widget.tweetVM.getDisplayTweet().videoUrls.values.last
         : widget.tweetVM.getDisplayTweet().videoUrls.values.first;
-    controller = BetterPlayerController(
-      betterPlayerConfiguration,
-      betterPlayerDataSource: BetterPlayerDataSource.network(
-        videoUrl,
-        qualities: widget.tweetVM.getDisplayTweet().videoUrls,
-      ),
-    );
-    controller.setVolume(widget.initialVolume!);
+
+    if (kIsWeb) {
+      _vpController = VideoPlayerController.network(videoUrl)
+        ..initialize().then((_) {
+          // Ensure the first frame is shown after the video is initialized, even before the play button has been pressed.
+          setState(() {});
+        });
+    } else {
+      betterPlayerConfiguration = BetterPlayerConfiguration(
+        placeholder: const Center(
+          child: SizedBox(
+            height: 32,
+            width: 32,
+            child: CircularProgressIndicator(),
+          ),
+        ),
+        errorBuilder: (context, message) {
+          return const Text('Error while loading video :-(');
+        },
+        aspectRatio: widget.tweetVM.getDisplayTweet().videoAspectRatio,
+        controlsConfiguration: BetterPlayerControlsConfiguration(
+          enablePlaybackSpeed: false,
+          enableSkips: false,
+          enableMute: !widget.tweetVM.getDisplayTweet().hasGif,
+          showControls: !widget.tweetVM.getDisplayTweet().hasGif,
+          enableSubtitles: false,
+          enableFullscreen: widget.enableFullscreen,
+        ),
+        allowedScreenSleep: false,
+        deviceOrientationsOnFullScreen: [
+          DeviceOrientation.portraitUp,
+          DeviceOrientation.landscapeLeft,
+          DeviceOrientation.landscapeRight
+        ],
+        fullScreenAspectRatio:
+            widget.tweetVM.getDisplayTweet().videoAspectRatio,
+        autoPlay: widget.tweetVM.getDisplayTweet().hasGif || widget.autoPlay,
+        looping: widget.tweetVM.getDisplayTweet().hasGif,
+        overlay: Padding(
+          padding: const EdgeInsets.only(
+            left: 4.0,
+          ),
+          child: widget.tweetVM.getDisplayTweet().hasGif
+              ? Align(
+                  alignment: Alignment.bottomLeft,
+                  child: Image.asset(
+                    'assets/tw__ic_gif_badge.png',
+                    fit: BoxFit.fitWidth,
+                    package: 'tweet_ui',
+                    height: 16,
+                    width: 16,
+                  ),
+                )
+              : Container(),
+        ),
+      );
+      controller = BetterPlayerController(
+        betterPlayerConfiguration,
+        betterPlayerDataSource: BetterPlayerDataSource.network(
+          videoUrl,
+          qualities: widget.tweetVM.getDisplayTweet().videoUrls,
+        ),
+      );
+      controller.setVolume(widget.initialVolume!);
+    }
   }
 
   @override
   Widget build(BuildContext context) {
     super.build(context);
 
-    // web or mobile, doesn't work on desktop
-    if (kIsWeb || Platform.isAndroid || Platform.isIOS) {
+    if (kIsWeb) {
+      return _vpController.value.isInitialized
+          ? AspectRatio(
+              aspectRatio: _vpController.value.aspectRatio,
+              child: Stack(
+                children: [
+                  VideoPlayer(_vpController),
+                  InkWell(
+                    onTap: () {
+                      setState(() {
+                        _vpController.value.isPlaying
+                            ? _vpController.pause()
+                            : _vpController.play();
+                      });
+                    },
+                  ),
+                ],
+              ),
+            )
+          : Container();
+    } else if (Platform.isAndroid || Platform.isIOS) {
       return BetterPlayer(
         controller: controller,
       );
